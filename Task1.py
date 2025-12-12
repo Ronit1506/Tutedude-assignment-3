14b1a5b40ed42ceceff6b4a71540986a51896a1e
Ronit1506 <ronit15102004@gmail.com>
2025-12-13 01:17:39 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+from math import factorial
+
+
+def factorial(num):
+    fact=1
+    while num>1:
+        fact*=num
+        num-=1
+    return fact
+num=int(input("Enter a number:"))
+print(f"Factorial of {num} is :",factorial(num))
---
+++ Task2.py
@@ -0,0 +1,12 @@
+import math
+
+from Scripts.arithmetic import square_root
+
+num=int(input("Enter a number :"))
+squareroot= math.sqrt(num)
+logarithm=math.log(num)
+sine=math.sin(num)
+
+print("Square root:",squareroot)
+print("logarithm:",logarithm)
+print("sine:",sine)
